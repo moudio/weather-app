99550220aa8f8a9963faa5f5bf41c6d34f279f86
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-02-07 15:46:49 +0000

    Correcting the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require('path'); // eslint-disable-line global-require
 
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   watch: true,
   module: {
@@ -14,29 +14,29 @@ module.exports = {
         use: [
           {
             // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
             // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
+            loader: 'css-loader',
           },
           {
             // Loader for webpack to process CSS with PostCSS
             loader: 'postcss-loader',
             options: {
-              plugins: function () {
+              plugins() {
                 return [
-                  require('autoprefixer')
+                  require('autoprefixer'),
                 ];
-              }
-            }
+              },
+            },
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
-  }
-}
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
