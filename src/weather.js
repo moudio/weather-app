a259c7fa6e203438cb99167cb617adc76bc81f7c
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-04-21 14:31:56 +0000

    Adding https to the api

--- src/weather.js
+++ src/weather.js
@@ -5,13 +5,13 @@ export default class Weather {
   }
 
   async getWeatherCelcius() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&APPID=${this.apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&APPID=${this.apiKey}`);
     const data = await response.json();
     return data;
   }
 
   async getWeatherFahrenheit() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=imperial&APPID=${this.apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=imperial&APPID=${this.apiKey}`);
     const data = await response.json();
     return data;
   }
